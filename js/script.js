00708d10e317ad014f9d6ad3c8c27dba2cf38fcd
Macbook <macbook@Macbook-Air.local>
2024-11-05 16:40:38 +0500

    fixed small errors

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@
 document.querySelector(".mobile-btn").addEventListener("click", function () {
   document.querySelector(".menu").classList.toggle("active");
 });
+
+function slide(direction) {
+  const sliderWrapper = document.querySelector(".slider-wrapper");
+  const cardWidth = document.querySelector(".pricing-card").offsetWidth;
+  sliderWrapper.scrollBy({
+    left: direction * (cardWidth + 16), // Adjust as needed for spacing between cards
+    behavior: 'smooth'
+  });
+}
